d7cd1e061c3812851cab55e8e9ba93fac175ccfb
amufid <asho3305@gmail.com>
2021-10-10 13:15:47 +0700

    first commit

--- app/src/main/java/com/example/pertemuan_1/MainActivity.java
+++ app/src/main/java/com/example/pertemuan_1/MainActivity.java
@@ -14,9 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void biodata(View view){
         Intent a = new Intent(MainActivity.this, Biodata.class);
         startActivity(a);
     }
 
+    public void segitiga(View view){
+        Intent b = new Intent(MainActivity.this,Segitiga.class);
+        startActivity(b);
+    }
 }
